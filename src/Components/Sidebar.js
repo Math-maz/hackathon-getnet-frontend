d8af90b4795c4ef7f665622544b121402f2e343e
Matheus Araujo <mazmerox@gmail.com>
2020-10-12 11:12:28 -0300

    fix Link to cursos

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,80 +1,80 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import Logo from "../assets/styles/images/logo1.png";
-import Divider from '@material-ui/core/Divider';
-import HomeIcon from '@material-ui/icons/Home';
-import ContactsIcon from '@material-ui/icons/Contacts';
-import CastForEducationIcon from '@material-ui/icons/CastForEducation';
-import StarIcon from '@material-ui/icons/Star';
-import ShowChartIcon from '@material-ui/icons/ShowChart';
-import SettingsIcon from '@material-ui/icons/Settings';
-import MyStyles from '../assets/styles/MyStyles.js'
+import Divider from "@material-ui/core/Divider";
+import HomeIcon from "@material-ui/icons/Home";
+import ContactsIcon from "@material-ui/icons/Contacts";
+import CastForEducationIcon from "@material-ui/icons/CastForEducation";
+import StarIcon from "@material-ui/icons/Star";
+import ShowChartIcon from "@material-ui/icons/ShowChart";
+import SettingsIcon from "@material-ui/icons/Settings";
+import MyStyles from "../assets/styles/MyStyles.js";
 import { makeStyles } from "@material-ui/styles";
 
 const useStyles = makeStyles({
   root: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    width: '400px',
-    height: '100vh',
-    backgroundColor: 'white',
-    boxShadow: '6px 4px 6px #00000029',
-    opacity: '1'
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    width: "400px",
+    height: "100vh",
+    backgroundColor: "white",
+    boxShadow: "6px 4px 6px #00000029",
+    opacity: "1",
   },
-  logo: { 
-    width: "150px" 
+  logo: {
+    width: "150px",
   },
   logoContainer: {
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
-    height: '88px',
+    height: "88px",
   },
   menu: {
-    display: 'flex',
-    flexDirection: 'column',
-    fontFamily: 'Montserrat',
-    fontSize: '20px',
-    marginLeft: '19px',
-    width: '227px',
-    height: '350px'
+    display: "flex",
+    flexDirection: "column",
+    fontFamily: "Montserrat",
+    fontSize: "20px",
+    marginLeft: "19px",
+    width: "227px",
+    height: "350px",
   },
   homeButton: {
-    marginTop: '10px',
-    '& a': {
-      width: '100%',
-      textDecoration: 'none',
+    marginTop: "10px",
+    "& a": {
+      width: "100%",
+      textDecoration: "none",
       color: MyStyles.colors.secondary,
-      '& svg': {
-        verticalAlign: '-3px',
-        marginRight: '10px'
-      }
-    }
+      "& svg": {
+        verticalAlign: "-3px",
+        marginRight: "10px",
+      },
+    },
   },
   buttons: {
-    marginTop: '10px',
-    '& a': {
-      width: '100%',
-      textDecoration: 'none',
+    marginTop: "10px",
+    "& a": {
+      width: "100%",
+      textDecoration: "none",
       color: MyStyles.colors.primary,
-      '& svg': {
-        verticalAlign: '-3px',
-        marginRight: '10px'
-      }
-    }
+      "& svg": {
+        verticalAlign: "-3px",
+        marginRight: "10px",
+      },
+    },
   },
   singOut: {
-    fontFamily: 'Montserrat',
-    fontSize: '20px',
-    marginLeft: '19px',
-    marginBottom: '19px',
-    '& a': {
-      width: '100%',
-      textDecoration: 'none',
+    fontFamily: "Montserrat",
+    fontSize: "20px",
+    marginLeft: "19px",
+    marginBottom: "19px",
+    "& a": {
+      width: "100%",
+      textDecoration: "none",
       color: MyStyles.colors.primary,
-    }
-  }
+    },
+  },
 });
 
 const SideBar = () => {
@@ -83,12 +83,16 @@ const SideBar = () => {
     <div className={classes.root}>
       <div>
         <div className={classes.logoContainer}>
-          <img src={Logo} className={classes.logo} alt="Duas mãos formando um círculo"/>
+          <img
+            src={Logo}
+            className={classes.logo}
+            alt="Duas mãos formando um círculo"
+          />
         </div>
         <Divider variant="middle" />
         <div className={classes.menu}>
           <div className={classes.homeButton}>
-            <Link to='/dashboard/'>
+            <Link to="/dashboard/">
               <HomeIcon />
               Início
             </Link>
@@ -100,7 +104,7 @@ const SideBar = () => {
             </Link>
           </div>
           <div className={classes.buttons}>
-            <Link to='dashboard/cursos'>
+            <Link to="/dashboard/cursos">
               <CastForEducationIcon />
               Cursos
             </Link>
@@ -108,14 +112,14 @@ const SideBar = () => {
           <div className={classes.buttons}>
             <Link>
               <StarIcon />
-              Assinatura        
+              Assinatura
             </Link>
           </div>
           <div className={classes.buttons}>
             <Link>
               <ShowChartIcon />
               Seu negócio
-            </Link> 
+            </Link>
           </div>
           <div className={classes.buttons}>
             <Link>
@@ -123,15 +127,13 @@ const SideBar = () => {
               Configurações
             </Link>
           </div>
-        </div>        
+        </div>
       </div>
       <div className={classes.singOut}>
-          <Link>
-            Sair
-          </Link>
-        </div>
+        <Link>Sair</Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default SideBar;
