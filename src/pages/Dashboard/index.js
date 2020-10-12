144f9c3bca87db32a5226c171d8395749aba67e0
Matheus Araujo <mazmerox@gmail.com>
2020-10-12 15:35:20 -0300

    Add position sticky to Sidebar

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Button from '@material-ui/core/Button';
-import Divider from '@material-ui/core/Divider';
-import Profile from '../../Components/Profile';
-import SideBar from '../../Components/Sidebar';
-import Coursecard from '../../Components/Coursecard';
-import ContactsIcon from '@material-ui/icons/Contacts';
-import LocalOfferIcon from '@material-ui/icons/LocalOffer';
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
+import Profile from "../../Components/Profile";
+import SideBar from "../../Components/Sidebar";
+import Coursecard from "../../Components/Coursecard";
+import ContactsIcon from "@material-ui/icons/Contacts";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
 import { makeStyles } from "@material-ui/styles";
-import MyStyles from '../../assets/styles/MyStyles';
+import MyStyles from "../../assets/styles/MyStyles";
 
 const clients = 5;
 const coupons = 15;
@@ -17,133 +17,134 @@ const visits = 2;
 const mocks = [
   {
     imageSource: "https://www.w3schools.com/css/paris.jpg",
-    title: "Como fazer o fluxo de caixa de seu estabelecimento"
+    title: "Como fazer o fluxo de caixa de seu estabelecimento",
   },
   {
     imageSource: "https://www.w3schools.com/css/paris.jpg",
-    title: "Como fazer o fluxo de caixa de seu estabelecimento"
+    title: "Como fazer o fluxo de caixa de seu estabelecimento",
   },
   {
     imageSource: "https://www.w3schools.com/css/paris.jpg",
-    title: "Como fazer o fluxo de caixa de seu estabelecimento"
+    title: "Como fazer o fluxo de caixa de seu estabelecimento",
   },
   {
     imageSource: "https://www.w3schools.com/css/paris.jpg",
-    title: "Como fazer o fluxo de caixa de seu estabelecimento"
+    title: "Como fazer o fluxo de caixa de seu estabelecimento",
   },
   {
     imageSource: "https://www.w3schools.com/css/paris.jpg",
-    title: "Como fazer o fluxo de caixa de seu estabelecimento"
+    title: "Como fazer o fluxo de caixa de seu estabelecimento",
   },
   {
     imageSource: "https://www.w3schools.com/css/paris.jpg",
-    title: "Como fazer o fluxo de caixa de seu estabelecimento"
-  }      
+    title: "Como fazer o fluxo de caixa de seu estabelecimento",
+  },
 ];
 
 const useStyles = makeStyles({
   root: {
-    display: 'flex',
-    flexDirection: 'row',
+    display: "flex",
+    flexDirection: "row",
   },
   contentArea: {
-    width: '-webkit-fill-available',
+    width: "-webkit-fill-available",
   },
   content: {
-    marginLeft: '30px',
-    marginTop: '-30px'
+    marginLeft: "30px",
+    marginTop: "-30px",
+    overflowY: "auto",
+    overflowX: "hidden",
   },
   bannerTitle: {
-    fontWeight: 'lighter',
-    color: MyStyles.colors.darks[50]
+    fontWeight: "lighter",
+    color: MyStyles.colors.darks[50],
   },
   courseCardContainer: {
-    display: 'flex',
-    flexDirection: 'row',
+    display: "flex",
+    flexDirection: "row",
   },
   moreCourses: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: '20px',
-    '& a': {
-      width: '100%',
-      textDecoration: 'none',
-      color: MyStyles.colors.primary,
-    }
-  },
-  notificationContainer: {
-    background: 'white',
-    borderRadius: '5px',
-    width: '1040px',
-    height: '120px',
-    marginTop: '10px',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    '& svg': {
-      display: 'flex',
-      alignItems: 'center',
-      color: MyStyles.colors.primary,
-      marginRight: '30px',
-      height: '40px',
-      width: '40px',
-    }
-  },
-  notification: {
-    display: 'flex',
+    display: "flex",
     alignItems: "center",
     justifyContent: "center",
-    width: '200px',
-    textAlign: 'center',
-    color:  MyStyles.colors.darks[50]
+    fontSize: "20px",
+    "& a": {
+      width: "100%",
+      textDecoration: "none",
+      color: MyStyles.colors.primary,
+    },
+  },
+  notificationContainer: {
+    background: "white",
+    borderRadius: "5px",
+    width: "1040px",
+    height: "120px",
+    marginTop: "10px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    "& svg": {
+      display: "flex",
+      alignItems: "center",
+      color: MyStyles.colors.primary,
+      marginRight: "30px",
+      height: "40px",
+      width: "40px",
+    },
+  },
+  notification: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "200px",
+    textAlign: "center",
+    color: MyStyles.colors.darks[50],
   },
   counterContainer: {
-    width: '1040px',
-    marginTop: '15px',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between'
+    width: "1040px",
+    marginTop: "15px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   visitsAndCouponsContainer: {
-    display: 'flex',
-    flexDirection: 'row',
+    display: "flex",
+    flexDirection: "row",
     alignItems: "center",
-    justifyContent: 'space-evenly',
-    background: 'white',
-    borderRadius: '5px',
-    height: '150px',
-    width: '400px',
-    textAlign: 'center',
-    color:  MyStyles.colors.darks[50],
+    justifyContent: "space-evenly",
+    background: "white",
+    borderRadius: "5px",
+    height: "150px",
+    width: "400px",
+    textAlign: "center",
+    color: MyStyles.colors.darks[50],
   },
   bestDayContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-evenly',
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-evenly",
     alignItems: "center",
-    background: 'white',
-    borderRadius: '5px',
-    height: '150px',
-    width: '590px',
+    background: "white",
+    borderRadius: "5px",
+    height: "150px",
+    width: "590px",
     color: MyStyles.colors.primary,
   },
   btnsContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-evenly',
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-evenly",
   },
   btns: {
-    display: 'flex',
+    display: "flex",
     alignItems: "center",
     justifyContent: "center",
-    fontWeight: 'bold',
-    fontSize: '12px',
-    color: 'white',
-    borderRadius: '20px',
-    width: '300px',
-
-  }
+    fontWeight: "bold",
+    fontSize: "12px",
+    color: "white",
+    borderRadius: "20px",
+    width: "300px",
+  },
 });
 
 const Dashboard = () => {
@@ -154,63 +155,97 @@ const Dashboard = () => {
       <div className={classes.contentArea}>
         <Profile />
         <main className={classes.content}>
-          <section style={{marginTop: '15px'}}>
+          <section style={{ marginTop: "15px" }}>
             <h2 className={classes.bannerTitle}>Novidades para</h2>
-            <h1 className={classes.bannerTitle} style={{color: MyStyles.colors.primary}}>Ateliê Dona Bonita</h1>
+            <h1
+              className={classes.bannerTitle}
+              style={{ color: MyStyles.colors.primary }}
+            >
+              Ateliê Dona Bonita
+            </h1>
             <div className={classes.courseCardContainer}>
-              {mocks.slice(0,3).map(mock => (
-                <Coursecard 
-                  imageSource={mock.imageSource}
-                  title={mock.title}
-                />
+              {mocks.slice(0, 3).map((mock) => (
+                <Coursecard imageSource={mock.imageSource} title={mock.title} />
               ))}
               <div className={classes.moreCourses}>
-                <Link to='/dashboard/cursos'>
-                  Ver mais
-                </Link>
+                <Link to="/dashboard/cursos">Ver mais</Link>
               </div>
             </div>
           </section>
-          <section style={{marginTop: '15px'}}>
+          <section style={{ marginTop: "15px" }}>
             <h1 className={classes.bannerTitle}>Lembretes</h1>
             <div className={classes.notificationContainer}>
               <div className={classes.notification}>
                 <ContactsIcon />
-                <p><span style={{fontWeight: "bold"}}>{clients}</span>{clients === 1? ` cliente está`: ` clientes estão`} esperando sua resposta no chat</p>
+                <p>
+                  <span style={{ fontWeight: "bold" }}>{clients}</span>
+                  {clients === 1 ? ` cliente está` : ` clientes estão`}{" "}
+                  esperando sua resposta no chat
+                </p>
               </div>
-              <Divider orientation="vertical" flexItem/>
+              <Divider orientation="vertical" flexItem />
               <div className={classes.notification}>
                 <LocalOfferIcon />
-                <p>Sua loja tem
-                  <div><span style={{fontWeight: "bold", color: 'black'}}>{coupons}</span></div> 
-                  {coupons ===1 ? ` cupom disponível`: ` cupons disponíveis`}</p>
+                <p>
+                  Sua loja tem
+                  <div>
+                    <span style={{ fontWeight: "bold", color: "black" }}>
+                      {coupons}
+                    </span>
+                  </div>
+                  {coupons === 1 ? ` cupom disponível` : ` cupons disponíveis`}
+                </p>
               </div>
-              <Divider orientation="vertical" flexItem/>
+              <Divider orientation="vertical" flexItem />
               <div className={classes.btnsContainer}>
-                <Button variant="contained" className={classes.btns} style={{backgroundColor: MyStyles.colors.secondary}}>CRIAR UMA PROMOÇÃO</Button>
-                <Button variant="contained" className={classes.btns} style={{backgroundColor: MyStyles.colors.primary}}>ENVIAR UMA COBRANÇA</Button>
+                <Button
+                  variant="contained"
+                  className={classes.btns}
+                  style={{ backgroundColor: MyStyles.colors.secondary }}
+                >
+                  CRIAR UMA PROMOÇÃO
+                </Button>
+                <Button
+                  variant="contained"
+                  className={classes.btns}
+                  style={{ backgroundColor: MyStyles.colors.primary }}
+                >
+                  ENVIAR UMA COBRANÇA
+                </Button>
               </div>
             </div>
           </section>
-          <section style={{marginTop: '15px'}}>
+          <section style={{ marginTop: "15px" }}>
             <h1 className={classes.bannerTitle}>Nos últimos 30 dias</h1>
             <div className={classes.counterContainer}>
               <div className={classes.visitsAndCouponsContainer}>
                 <div>
-                  <div style={{fontSize: '24px'}}>{visits}</div>
-                  <div style={{color: MyStyles.colors.primary}}>{visits === 1? `Visita`: `Visitas`} na sua página</div>
+                  <div style={{ fontSize: "24px" }}>{visits}</div>
+                  <div style={{ color: MyStyles.colors.primary }}>
+                    {visits === 1 ? `Visita` : `Visitas`} na sua página
+                  </div>
                 </div>
-                <Divider orientation="vertical" flexItem/>
+                <Divider orientation="vertical" flexItem />
                 <div>
-                  <div style={{fontSize: '24px'}}>{coupons}</div>
-                  <div style={{color: MyStyles.colors.primary}}>{coupons === 1? `Cupom usado`: `Cupons usados`}</div>
+                  <div style={{ fontSize: "24px" }}>{coupons}</div>
+                  <div style={{ color: MyStyles.colors.primary }}>
+                    {coupons === 1 ? `Cupom usado` : `Cupons usados`}
+                  </div>
                 </div>
               </div>
               <div className={classes.bestDayContainer}>
                 <p>O maior número de visitas em sua loja foi:</p>
-                <p style={{color: 'black', fontWeight: 'bold'}}>Quarta-feira</p>
+                <p style={{ color: "black", fontWeight: "bold" }}>
+                  Quarta-feira
+                </p>
                 <p>Que tal adicionar um cupom de desconto este dia?</p>
-                <Button variant="contained" className={classes.btns} style={{backgroundColor: MyStyles.colors.secondary}}>CRIAR UMA PROMOÇÃO</Button>              
+                <Button
+                  variant="contained"
+                  className={classes.btns}
+                  style={{ backgroundColor: MyStyles.colors.secondary }}
+                >
+                  CRIAR UMA PROMOÇÃO
+                </Button>
               </div>
             </div>
           </section>
@@ -218,6 +253,6 @@ const Dashboard = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Dashboard;
