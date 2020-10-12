c91feeb3c066fa3428abe691cc0ec8d2a841b8ca
Matheus Araujo <mazmerox@gmail.com>
2020-10-12 13:16:58 -0300

    add payment feature

--- src/pages/ClientChat/index.js
+++ src/pages/ClientChat/index.js
@@ -3,6 +3,9 @@ import { makeStyles } from "@material-ui/styles";
 import React from "react";
 import MyStyles from "../../assets/styles/MyStyles";
 import Navbar from "../../Components/BaseNavbar";
+import Api from "../../lib/Api";
+import HomeContext from "../../lib/context/home/HomeContext";
+import { Check } from "@material-ui/icons";
 const useStyles = makeStyles({
   root: {
     display: "flex",
@@ -86,10 +89,91 @@ const messageMock = [
 ];
 export default function ClientChat() {
   const classes = useStyles();
+  const [messages, setMessages] = React.useState([]);
+  const [text, setText] = React.useState("");
+  const [paymentDone, setPaymentDone] = React.useState(false);
+  const { state, actions } = React.useContext(HomeContext);
+  React.useEffect(() => {
+    setInterval(function () {
+      Api.get("/conversation/1eeeaff9-d1fb-40cd-a059-a53799ee15ca").then(
+        (res) => {
+          console.log(res);
+          if (res.data.isOk) {
+            setMessages(res.data.messages);
+          }
+        }
+      );
+    }, 3000);
+  }, []);
+  const handleBilling = (amount) => {
+    const formatedAmount = amount.slice(2);
+    console.log(formatedAmount);
+    Api.get(
+      `/transaction/e7804097-1234-43cc-9df2-9688200b12ad/a1036fae-07b2-4d75-907b-51d74c7878a6/${formatedAmount}/transacao`
+    ).then((res) => {
+      if (res.data.isOk) {
+        actions.setUser({ balance: res.data.total });
+        setPaymentDone(true);
+      }
+      console.log(res);
+    });
+  };
+  const formatMessage = (message) => {
+    if (message.text.includes("R$")) {
+      return (
+        <div
+          className={message.isMe ? classes.sended : classes.received}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div style={{ fontWeight: "bold", fontSize: 24 }}>
+            Cobrança de {message.text}
+          </div>
+          <button
+            style={{
+              width: "80%",
+              marginTop: "4px",
+              padding: "12px 0",
+              backgroundColor: MyStyles.colors.primary,
+              color: MyStyles.colors.white,
+              borderRadius: 25,
+              border: "none",
+              cursor: "pointer",
+            }}
+            onClick={() => handleBilling(message.text)}
+          >
+            {paymentDone ? (
+              <Check style={{ color: MyStyles.colors.white, fontSize: 14 }} />
+            ) : (
+              "PAGAR"
+            )}
+          </button>
+        </div>
+      );
+    }
+    return (
+      <div className={message.isMe ? classes.sended : classes.received}>
+        {message.text}
+      </div>
+    );
+  };
   const handleKeyDown = (e) => {
     if (e.keyCode === 13 && e.shiftKey === false) {
       e.preventDefault();
       console.log(e);
+      Api.post("/conversation/1eeeaff9-d1fb-40cd-a059-a53799ee15ca", {
+        isMe: true,
+        text,
+      }).then((res) => {
+        if (res.data.isOk) {
+          setMessages(res.data.messages);
+          setText("");
+        }
+      });
     }
   };
   return (
@@ -100,16 +184,15 @@ export default function ClientChat() {
           <div className={classes.header}>
             Você está conversando com Ateliê Dona Bonita (Joana)
           </div>
-          {messageMock.map((message) => (
-            <div className={message.isMe ? classes.sended : classes.received}>
-              {message.message}
-            </div>
-          ))}
+          {messages.length > 1 &&
+            messages.map((message) => formatMessage(message))}
         </div>
         <Paper elevation={4} className={classes.chatWindow}>
           <TextField
             label="Digite uma mensagem..."
             className={classes.textField}
+            value={text}
+            onChange={(e) => setText(e.target.value)}
             multiline={true}
             variant="filled"
             rows={4}
--- src/pages/StoreChat/index.js
+++ src/pages/StoreChat/index.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/styles";
 import React from "react";
 import MyStyles from "../../assets/styles/MyStyles";
 import Navbar from "../../Components/BaseNavbar";
+import Api from "../../lib/Api";
 const useStyles = makeStyles({
   root: {
     display: "flex",
@@ -101,26 +102,84 @@ const messageMock = [
 ];
 export default function ClientChat() {
   const classes = useStyles();
+  const [messages, setMessages] = React.useState([]);
+  const [text, setText] = React.useState("");
+  const [billing, setBilling] = React.useState("R$");
+  React.useEffect(() => {
+    setInterval(function () {
+      Api.get("/conversation/1eeeaff9-d1fb-40cd-a059-a53799ee15ca").then(
+        (res) => {
+          console.log(res);
+          if (res.data.isOk) {
+            setMessages(res.data.messages);
+          }
+        }
+      );
+    }, 3000);
+  }, []);
   const handleKeyDown = (e) => {
     if (e.keyCode === 13 && e.shiftKey === false) {
       e.preventDefault();
       console.log(e);
+      Api.post("/conversation/1eeeaff9-d1fb-40cd-a059-a53799ee15ca", {
+        isMe: false,
+        text,
+      }).then((res) => {
+        if (res.data.isOk) {
+          setMessages(res.data.messages);
+          setText("");
+        }
+      });
     }
+  };
+  const handleBilling = () => {
+    Api.post("/conversation/1eeeaff9-d1fb-40cd-a059-a53799ee15ca", {
+      isMe: false,
+      text: billing,
+    }).then((res) => {
+      if (res.data.isOk) {
+        setMessages(res.data.messages);
+        setBilling("R$");
+      }
+    });
+  };
+  const formatMessage = (message) => {
+    if (message.text.includes("R$")) {
+      return (
+        <div
+          className={!message.isMe ? classes.sended : classes.received}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div style={{ fontWeight: "bold", fontSize: 24 }}>
+            Cobrança de {message.text} enviada
+          </div>
+        </div>
+      );
+    }
+    return (
+      <div className={!message.isMe ? classes.sended : classes.received}>
+        {message.text}
+      </div>
+    );
   };
   return (
     <>
       <Navbar />
       <div className={classes.root}>
         <div className={classes.messageWindow}>
-          {messageMock.map((message) => (
-            <div className={message.isMe ? classes.sended : classes.received}>
-              {message.message}
-            </div>
-          ))}
+          {messages.length > 1 &&
+            messages.map((message) => formatMessage(message))}
         </div>
         <Paper elevation={4} className={classes.chatWindow}>
           <TextField
             label="Digite uma mensagem..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}
             className={classes.textField}
             multiline={true}
             variant="filled"
@@ -130,8 +189,12 @@ export default function ClientChat() {
           <div className={classes.billingContainer}>
             <input
               style={{ fontSize: 48, width: "50%", border: "1px solid #CCC" }}
+              value={billing}
+              onChange={(e) => setBilling(e.target.value)}
             />
-            <button className={classes.billingButton}>ENVIAR COBRANÇA</button>
+            <button onClick={handleBilling} className={classes.billingButton}>
+              ENVIAR COBRANÇA
+            </button>
           </div>
         </Paper>
       </div>
